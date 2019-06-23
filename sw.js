054c0825a1cdec8f547b70b21333fb2e52219efe
Lingjie Tao <lingjietao@Lingjies-MacBook-Air.local>
2019-06-23 23:29:33 +0200

    modified fetch

--- sw.js
+++ sw.js
@@ -2,36 +2,29 @@ let cache_name = "v1";
 
 self.addEventListener("install", function(event){
     event.waitUntil(
-        caches.open("v1").then(function(cache) {
-            return cache.addAll([
-                "/",
-                "/index.html",
-                "/restaurant.html",
-                "/css/styles.css",
-                "/js/",
-                "/data/restaurants.json",
-                "/js/dbhelper.js",
-                "/js/main.js",
-                "/js/restaurant_info.js",
-                "/js/register.js",
-                "/img"
-            ]).catch(function(err){
-                console.log("caches open failed: " + err);
-            });
-        })
+        caches.open(cache_name)
+            .then(function(cache) {
+                console.log("cache opened");
+                return cache.addAll([
+                    "/index.html",
+                    "/restaurant.html",
+                    "/css/styles.css",
+                    "/js/dbhelper.js",
+                    "/js/main.js",
+                    "/js/restaurant_info.js",
+                ]).catch(function(err){
+                    console.log("caches open failed: " + err);
+                });
+            })
     );
 });
 
-self.addEventListener('fetch', function(event) {
+self.addEventListener("fetch", function(event) {
     event.respondWith(
       caches.match(event.request)
         .then(function(response) {
-          // Cache hit - return response
-          if (response) {
-            return response;
-          }
-          return fetch(event.request);
-        }
-      )
+            console.log("cache fetched");
+            return response || fetch(event.request);
+        })
     );
 });

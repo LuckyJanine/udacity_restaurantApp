5e08189f3f1cbee70147a1051fdbc9dc00218509
Lingjie Tao <lingjietao@Lingjies-MacBook-Air.local>
2019-06-28 22:09:34 +0200

    changed service worker

--- sw.js
+++ sw.js
@@ -26,6 +26,7 @@ self.addEventListener('activate', function(event) {
   });
 
 self.addEventListener('fetch', function(event) {
+    console.log(event.request.url);
     event.respondWith(
         caches.match(event.request)
         .then(function(response) {   
